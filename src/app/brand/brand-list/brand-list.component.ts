a8e6b5da95022891fec3cc255a862582471581a2
nani17891 <75967482+nani17891@users.noreply.github.com>
2021-06-07 11:53:11 +0530

    Update brand-list.component.ts

--- src/app/brand/brand-list/brand-list.component.ts
+++ src/app/brand/brand-list/brand-list.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CreateBrandDialogComponent } from '../create-brand-dialog/create-brand-dialog.component';
-import {EditBrandDialogComponent} from '../edit-brand-dialog/edit-brand-dialog.component';
-import {ManageBrandDialogComponent} from '../manage-brand-dialog/manage-brand-dialog.component';
-import {DeleteBrandDialogComponent} from '../delete-brand-dialog/delete-brand-dialog.component';
-import {  FormControl } from '@angular/forms';
+import { EditBrandDialogComponent } from '../edit-brand-dialog/edit-brand-dialog.component';
+import { ManageBrandDialogComponent } from '../manage-brand-dialog/manage-brand-dialog.component';
+import { DeleteBrandDialogComponent } from '../delete-brand-dialog/delete-brand-dialog.component';
+import { FormControl } from '@angular/forms';
+import { CreatePlanningCycleDialogComponent } from '../create-planning-cycle-dialog/create-planning-cycle-dialog.component';
 import { BrandService } from '../brand.service';
-import { Observable } from 'rxjs';
- 
+import {Observable} from 'rxjs'
+
 @Component({
   selector: 'app-brand-list',
   templateUrl:'./brand-list.component.html',
   styleUrls: ['./brand-list.component.css']
 })
 export class BrandListComponent implements OnInit {
-  public brandList :any = [];
+  public brandList: any = [];
   searchText = new FormControl();
-  constructor(public dialog: MatDialog,
-              public brandService: BrandService) { }
+  constructor(public dialog: MatDialog,public brandService:BrandService) { }
 
   ngOnInit(): void {
-    this.brandService.getBrands().subscribe((response) => {
-      this.brandList = response;
-       
-    })    
-
-  	/*this.brandList = [
-  	{
+	  this.brandService.getBrands().subscribe(response=>this.brandList=response)
+/*
+  	this.brandList = [
+		  
+ 	{
+  
       id: 1,
   		name: "Brand Cam",
   		description: "Description about your brand",
@@ -58,7 +57,7 @@ export class BrandListComponent implements OnInit {
   	},
 
 
-  	];*/
+    ];*/
   }
 
   generateTooltip(currentBrandObj) {
@@ -75,10 +74,7 @@ export class BrandListComponent implements OnInit {
     dialogConfig.autoFocus = true;
     dialogConfig.width = "500px";
     dialogConfig.height = "600px";
-    dialogConfig.data = {
-        id: 1,
-        title: 'Angular For Beginners'
-    };
+     
 
     const dialogRef =  this.dialog.open(CreateBrandDialogComponent, dialogConfig);
    
@@ -86,34 +82,37 @@ export class BrandListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
       if(result) {
-        this.brandService.createBrand(result).subscribe((response) => {
+        this.brandService.createBrand(result).subscribe((_response) => {
           this.brandList.push(result);
         })    
       }
       
     });
   }
-
   editBrandDialog(brandObject){
+
+    
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = true;
     dialogConfig.width = "500px";
-    dialogConfig.height = "400px";
-    dialogConfig.data = brandObject;
+    dialogConfig.height = "500px";
+    
+    this.brandService.getBrand(brandObject.id).subscribe((response) => {
+          dialogConfig.data = response[0];
+          const dialogRef =  this.dialog.open(EditBrandDialogComponent, dialogConfig);
 
-    const dialogRef =  this.dialog.open(EditBrandDialogComponent, dialogConfig);
-   
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-       this.brandService.updateBrand(result).subscribe((response) => {
-          var index = this.brandList.findIndex(item => item.id === result.id)
-          this.brandList.splice(index, 1, result)
-        })   
-    });
-  }
+ 
+          dialogRef.afterClosed().subscribe(result => {
+              console.log(`Dialog result: ${result}`);
+               this.brandService.updateBrand(result).subscribe((_response) => {
+                  var index = this.brandList.findIndex(item => item.id === result.id)
+                  this.brandList.splice(index, 1, result)
+                })   
+            });
+          });
+ }
 
 	manageBrandDialog(){
 		const dialogConfig = new MatDialogConfig();
@@ -135,24 +134,43 @@ export class BrandListComponent implements OnInit {
 		
 		});
 	}
-		deleteBrandDialog(){
-			const dialogConfig = new MatDialogConfig();
-		
-			dialogConfig.disableClose = false;
-			dialogConfig.autoFocus = true;
-			dialogConfig.width = "500px";
-			dialogConfig.height = "300px";
-			dialogConfig.data = {
-				id: 1,
-				title: 'Angular For Beginners'
-			};
-		
-			const dialogRef =  this.dialog.open(DeleteBrandDialogComponent, dialogConfig);
-		   
-		
-			dialogRef.afterClosed().subscribe(result => {
-			  console.log(`Dialog result: ${result}`);
-			  
-			});
-		}
+  
+	deleteBrandDialog(_brandObj){
+		const dialogConfig = new MatDialogConfig();
+	
+		dialogConfig.disableClose = false;
+		dialogConfig.autoFocus = true;
+		dialogConfig.width = "500px";
+		dialogConfig.height = "400px";
+		dialogConfig.data = {
+			id: 1,
+			title: 'Angular For Beginners'
+		};
+	
+		const dialogRef =  this.dialog.open(DeleteBrandDialogComponent, dialogConfig);
+	   
+	
+		dialogRef.afterClosed().subscribe(result => {
+		  console.log(`Dialog result: ${result}`);
+      this.brandService.deleteBrand(_brandObj).subscribe((_response) => {
+        var index = this.brandList.findIndex(item => item.id === result.id)
+        this.brandList.slice(index, 1, result)
+      })
+		  
+		});
+	}
+
+openNewPlanningCycleDialog(brand) {
+  const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "600px";
+    dialogConfig.height = "90vh";
+    dialogConfig.data = brand;
+    const dialogRef = this.dialog.open(CreatePlanningCycleDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      brand.planCycle.push(result);
+    });
+  }
+
 }

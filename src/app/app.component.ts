91665418d7e64b3a88e3d9ba5affa108163ce8cf
nani17891 <75967482+nani17891@users.noreply.github.com>
2021-06-11 06:10:20 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { RestApiService } from './common/rest-api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'RBP';
+  userlinks:any[];
+  applicationlinks:any[];
+  constructor(public restApi : RestApiService){
+    this.restApi.post("/user_session/",{ Return_URL: location.href }).subscribe(response=>{
+      let htmlContent = response["htmlContent"];
+      this.userlinks = this.getLinks(htmlContent, "zs-link");
+      this.applicationlinks = this.getLinks(htmlContent, "lbar-link");
+    })
+  }
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  getLinks(htmlContent: string, htmlClass) {
+    let s = document.createElement('div');
+    s.innerHTML = htmlContent;
+    let links = s.getElementsByClassName(htmlClass);
+    let arr = [];
+    for (let i = 0; i < links. length; i++) {
+      var data = {link:links[i].getAttribute("href"), text: links[i].textContent} 
+      arr.push(data);
+    }
+    return arr;
+  }
+
 }
+  
